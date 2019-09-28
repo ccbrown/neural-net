137dac62b5a05b6f56859b4ba119d8451c77358a
Christopher Brown <ccbrown112@gmail.com>
2019-09-28 13:08:32 -0400

    another quick comment update

--- src/algebra/mod.rs
+++ src/algebra/mod.rs
@@ -30,7 +30,7 @@ pub mod constant; pub use constant::*;
 // if square didn't exist, we could just as easily write "x * x". However, `format!("{}",
 // x.square())` is dramatically easier to read than `format!("{}", x * x)` when x is a complex
 // expression. There are also potential performance benefits to having more specialized operations
-// implemented, but for now the difference is pretty negligible.
+// implemented.
 #[derive(Clone)]
 pub struct Expr {
     expr: Rc<ExprImpl>,
